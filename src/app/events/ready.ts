7c3ebd3b6aa9e111e71ebf4a193f4616a10b4279
matheo-1712 <perod.matheo@gmail.com>
2026-05-24 04:21:11 +0200

    Remove unused

--- src/app/events/ready.ts
+++
@@ -1,9 +0,0 @@
-import {Events, Client} from "discord.js";
-
-module.exports = {
-    name: Events.ClientReady,
-    once: true,
-    execute(client: Client) {
-        client.emit('clientReady');
-    },
-};
